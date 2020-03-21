a22920037d68df693349f7e845269c0d20c16c43
Yogesh Chavan <ychavan@mdsol.com>
2020-03-21 16:17:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-function App() {
+export default function App() {
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [errorMsg, setErrorMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const validateInput = () => {
+    const fields = [
+      {
+        name: 'email',
+        value: state.email,
+        message: 'Email address should be not be blank.'
+      },
+      {
+        name: 'password',
+        value: state.password,
+        message: 'Password should be not be blank.'
+      }
+    ];
+    const isNotFilled = fields.some(field => {
+      if (field.value.trim() === '') {
+        setErrorMsg(field.message);
+        field.name === 'email'
+          ? emailRef.current.focus()
+          : passwordRef.current.focus();
+        return true;
+      }
+      setErrorMsg('');
+      return false;
+    });
+    return isNotFilled;
+  };
+
+  const handleOnSubmit = event => {
+    event.preventDefault();
+    const isInvalid = validateInput();
+    if (!isInvalid) {
+      setSuccessMsg("You're good to go!");
+    } else {
+      setSuccessMsg('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Login Page</h1>
+      <div className="col-md-6 offset-md-3">
+        {successMsg && <p className="successMsg">{successMsg}</p>}
+        {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+        <Form onSubmit={handleOnSubmit}>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              ref={emailRef}
+              value={state.username}
+              placeholder="Enter your email address"
+              autoComplete="off"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="text"
+              name="password"
+              ref={passwordRef}
+              value={state.password}
+              placeholder="Enter your password"
+              autoComplete="off"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
-
-export default App;
